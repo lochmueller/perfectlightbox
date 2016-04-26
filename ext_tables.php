cda9d52231bca6d7737abc32d2eedaf1bdc3567d
Tim Lochmüller <tim@fruit-lab.de>
2016-04-26 12:55:05 +0200

    Move TCA manipulation to Overrides

--- ext_tables.php
+++ ext_tables.php
@@ -2,40 +2,6 @@
 if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
-$tempColumns = [
-    "tx_perfectlightbox_activate"     => [
-        "exclude" => 1,
-        "label"   => "LLL:EXT:perfectlightbox/locallang_db.xml:tt_content.tx_perfectlightbox_activate",
-        "config"  => [
-            "type" => "check",
-        ]
-    ],
-    "tx_perfectlightbox_imageset"     => [
-        "exclude" => 1,
-        "label"   => "LLL:EXT:perfectlightbox/locallang_db.xml:tt_content.tx_perfectlightbox_imageset",
-        "config"  => [
-            "type" => "check",
-        ]
-    ],
-    "tx_perfectlightbox_presentation" => [
-        "exclude" => 1,
-        "label"   => "LLL:EXT:perfectlightbox/locallang_db.xml:tt_content.tx_perfectlightbox_presentation",
-        "config"  => [
-            "type" => "check",
-        ]
-    ],
-    "tx_perfectlightbox_slideshow"    => [
-        "exclude" => 1,
-        "label"   => "LLL:EXT:perfectlightbox/locallang_db.xml:tt_content.tx_perfectlightbox_slideshow",
-        "config"  => [
-            "type" => "check",
-        ]
-    ],
-];
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns("tt_content", $tempColumns, 1);
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'static/', 'Perfect Lightbox');
 
-$GLOBALS['TCA']['tt_content']['palettes']['7']['showitem'] .= ', tx_perfectlightbox_activate, tx_perfectlightbox_imageset, tx_perfectlightbox_presentation, tx_perfectlightbox_slideshow';
-# BEN: Quickfix for TYPO3 4.5
-$GLOBALS['TCA']['tt_content']['palettes']['imagelinks']['showitem'] .= ', tx_perfectlightbox_activate, tx_perfectlightbox_imageset, tx_perfectlightbox_presentation, tx_perfectlightbox_slideshow';
